df83c7b0032571df890e26226c44708d3263ac09
farhat eleuch <dainload5@gmail.com>
2022-01-06 20:47:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './compannat/NavBar';
-import Form0 from './compannat/Form0';
+import React from 'react'
+import './style.css'
+import myimage from './2.jpg'
+
 function App() {
+ let name='shlomo'
   return (
-    <div className="App">
-      <NavBar/>
-    <Form0/>
+    <div>
+      <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+<h1 className="title red">Your name here:{name}</h1>
+<br />
+
+<img src={myimage}/>
+
+<br/>
+
+<img src="./1.jpg"/>
+
+
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type="video/mp4"/ >
+
+</video>
+
+
     </div>
-  );
+
+
+  )
 }
 
-export default App;
+export default App
