42ca2ad65d7941bf06929c74c38b8a6bd35266ad
farhat eleuch <dainload5@gmail.com>
2022-01-06 19:30:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './compannat/NavBar';
+import Form0 from './compannat/Form0';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    <Form0/>
+    </div>
+  );
+}
+
+export default App;
